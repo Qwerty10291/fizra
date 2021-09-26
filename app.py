2b71b385bc75b1afd0bca25fe65161c8cdbf5a65
Andrey <andrtazet@gmail.com>
2021-09-26 19:19:39 +0300

    final

--- app.py
+++ app.py
@@ -5,8 +5,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     diploms = [diplom for diplom in os.listdir('./static/images/') if diplom.startswith('diplom')]
-    
     return render_template('index.html', diploms=diploms)
+
+@app.route('/pupils')
+def pupils():
+    return render_template('pupils.html')
+
+@app.route('/manuals')
+def manuals():
+    return render_template('manual.html')
 
 if __name__ == '__main__':
     app.run('localhost', 8000, debug=True)
